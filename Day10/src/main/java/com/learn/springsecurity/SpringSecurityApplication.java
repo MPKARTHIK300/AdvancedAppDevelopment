a1b55a928704d4d27f6950885e325236cc5b021c
MPKARTHIK300 <125946355+MPKARTHIK300@users.noreply.github.com>
2024-02-09 11:46:06 +0530

    Add files via upload

---
+++ Day10/src/main/java/com/learn/springsecurity/SpringSecurityApplication.java
@@ -0,0 +1,31 @@
+package com.learn.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@SpringBootApplication
+@OpenAPIDefinition(
+		info =@Info(
+				title = "App Title",
+				version = "1.1.2",
+				description = "App Description",
+				contact = @Contact(
+						name = "Author Name",
+						email = "Author Email"
+						)
+				)
+		)
+
+@Tag(name="mpk", description="mpk")
+public class SpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityApplication.class, args);
+	}
+
+}
