42d2791f792a3d0f88bb62146771a9db6aad1cd4
MPKARTHIK300 <125946355+MPKARTHIK300@users.noreply.github.com>
2024-02-09 11:49:52 +0530

    Add files via upload

---
+++ Day12/src/main/java/com/bec/becenquiry/Services/signupService.java
@@ -0,0 +1,28 @@
+// package com.bec.becenquiry.Services;
+
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.stereotype.Service;
+
+// import com.bec.becenquiry.Entity.signupentity;
+// import com.bec.becenquiry.Repository.signupRepo;
+
+
+
+// @Service
+// public class signupService {
+
+//     @Autowired
+//     private signupRepo userRepository;
+
+//     public signupentity findByUsername(String username) {
+//         return userRepository.findByUsername(username);
+//     }
+
+//     public signupentity findByEmail(String email) {
+//         return userRepository.findByEmail(email);
+//     }
+
+//     public signupentity saveUser(signupentity user) {
+//         return userRepository.save(user);
+//     }
+// }
