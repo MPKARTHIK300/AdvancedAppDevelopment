61b8ca30536d353185011f7ddd0abe78fff95660
MPKARTHIK300 <125946355+MPKARTHIK300@users.noreply.github.com>
2024-02-09 11:41:57 +0530

    Add files via upload

---
+++ Day9/src/main/java/com/bec/becenquiry/Repository/signupRepo.java
@@ -0,0 +1,12 @@
+// package com.bec.becenquiry.Repository;
+
+// import org.springframework.data.jpa.repository.JpaRepository;
+
+// import com.bec.becenquiry.Entity.signupentity;
+
+// public interface signupRepo extends JpaRepository<signupentity, String> {
+
+//     signupentity findByUsername(String username);
+
+//     signupentity findByEmail(String email);
+// }
